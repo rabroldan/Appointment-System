7ef7bc9643d7d97661359c84ec4a2b7bd68b2964
rabroldan <141264553+rabroldan@users.noreply.github.com>
2023-08-02 13:34:29 -0400

    Update a1ms3.c

--- a1ms3.c
+++ a1ms3.c
@@ -1,3 +1,15 @@
+/*
+*****************************************************************************
+                       
+Full Name  : Ronald Roldan
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
 
 #include <stdio.h>
 
