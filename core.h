826fde424d8085b94ef2cbc0346d4ab619c0de10
rabroldan <141264553+rabroldan@users.noreply.github.com>
2023-08-02 13:35:04 -0400

    Update core.h

--- core.h
+++ core.h
@@ -1,10 +1,7 @@
 /*
 *****************************************************************************
-                        Assignment 1 - Milestone 3
-Full Name  :
-Student ID#:
-Email      :
-Section    :
+                       
+Full Name  : Ronald Roldan
 
 Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
@@ -12,28 +9,6 @@ shared with any other student or 3rd party content provider. This submitted
 piece of work is entirely of my own creation.
 *****************************************************************************
 */
-
-// SAFE-GUARD: 
-// It is good practice to apply safe-guards to header files
-// Safe-guard's ensures only 1 copy of the header file is used in the project build
-// The macro name should be mirroring the file name with _ for spaces, dots, etc.
-
-// !!! DO NOT DELETE THE BELOW 2 LINES !!!
-#ifndef CORE_H
-#define CORE_H
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 
-// Copy your work done from Milestone #2 (core.h) into this file
-// 
-// NOTE:
-// - Organize your functions into the below categories
-// - Make sure the core.c file also lists these functions in the same order!
-// - Be sure to provide a BRIEF comment for each function prototype
-// - The comment should also be copied to the core.c function definition
-//
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 
 //////////////////////////////////////
 // USER INTERFACE FUNCTIONS
