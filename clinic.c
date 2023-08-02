d7002056040d69c5b3e81fd127bf04b00c88acf3
rabroldan <141264553+rabroldan@users.noreply.github.com>
2023-08-02 13:34:37 -0400

    Update clinic.c

--- clinic.c
+++ clinic.c
@@ -1,4 +1,9 @@
-/*Authenticity Declaration:
+/*
+*****************************************************************************
+                       
+Full Name  : Ronald Roldan
+
+Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
 shared with any other student or 3rd party content provider. This submitted
 piece of work is entirely of my own creation.
